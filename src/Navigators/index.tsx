bd91ab7dbe16f7d22ae21234f28d55e7e4536f4b
Mehmet Salih Akcan <akcanmsa@gmail.comn>
2023-04-24 10:27:47 +0300

    changed start screen

--- src/Navigators/index.tsx
+++ src/Navigators/index.tsx
@@ -26,7 +26,7 @@ export default function Navigators() {
       <StatusBar translucent backgroundColor="transparent" />
       <StatusBar barStyle={isDarkMode ? 'dark-content' : 'light-content'} />
       <Stack.Navigator
-        initialRouteName={ScreenNames.EXAM_SCREEN}
+        initialRouteName={ScreenNames.HOME_SCREEN}
         screenOptions={{
           headerShown: false,
         }}>
